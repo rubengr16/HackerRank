9a25878a738bf53a0bcca4fecfaa56a232e349b3
rubengr16 <ruben16gallego@gmail.com>
2021-01-04 20:02:50 +0100

    6 days comments plus constraint

--- 30_days_of_code/Day6/ReviewLoop.java
+++ 30_days_of_code/Day6/ReviewLoop.java
@@ -3,31 +3,42 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
+/*
+ * Reads a number T from input in an inclusive range of 1 to 10,
+ * later scans T lines from input each S in a subsequent line,
+ * S must have a length in an inclusive range of 2 to 10000.
+ * Once read the T strings, each one must be printed with the characters on the even positions on the left,
+ * separated by a space (" ") and on the right the odd ones.
+ */
 public class ReviewLoop {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         
-        Scanner scan = new Scanner(System.in);
-        int T = scan.nextInt();
-        scan.nextLine();
-        String[] S = new String[T];
-        for (int i = 0; i < T; i++) {
-            S[i] = scan.nextLine();
-        }
-        
-        
-        for (int i = 0; i < S.length; i++) {
-            char [] c = S[i].toCharArray();
-            for (int j = 0; j < c.length; j = j + 2) {
-                System.out.print(c[j]);
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();  // Reads T, number of lines which will be entered
+        if (1 <= T && T <= 10){  // Constraint 1 <= T <= 10
+            scanner.nextLine();  // Skips the end of the line where T was introduced
+            String[] S = new String[T];  // String array for saving the lines
+            for (int i = 0; i < T; i++) {  // Reading of lines
+                S[i] = scanner.nextLine();
             }
-            System.out.print(" ");
-            for (int j = 1; j < c.length; j = j + 2) {
-                System.out.print(c[j]);
+            
+            
+            for (int i = 0; i < T; i++) {  // The loop will iterate T times
+                char [] c = S[i].toCharArray();
+                if (2 <= c.length && c.length <= 10000){  // Constraint 2 <= S <= 10000
+                    for (int j = 0; j < c.length; j = j + 2) {  // Odd positions
+                        System.out.print(c[j]);
+                    }
+                    System.out.print(" ");  // Separator between even and odd positions
+                    for (int j = 1; j < c.length; j = j + 2) {  // Even positions
+                        System.out.print(c[j]);
+                    }
+                    System.out.println();  // Line separator
+                }
             }
-            System.out.println();
         }
+        scanner.close();
     }
 }
--- 30_days_of_code/Day6/ReviewLoopRemake.java
+++ 30_days_of_code/Day6/ReviewLoopRemake.java
@@ -3,30 +3,42 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
+/*
+ * Reads a number T from input in an inclusive range of 1 to 10,
+ * later scans T lines from input each S in a subsequent line,
+ * S must have a length in an inclusive range of 2 to 10000.
+ * Once read the T strings, each one must be printed with the characters on the even positions on the left,
+ * separated by a space (" ") and on the right the odd ones.
+ */
 public class ReviewLoopRemake {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         
-        Scanner scan = new Scanner(System.in);
-        int T = scan.nextInt();
-        scan.nextLine();
-        String[] S = new String[T];
-        for (int i = 0; i < T; i++) {
-            S[i] = scan.nextLine();
-        }
-        
-        
-        for (int t = 0; t < T; t++) {
-            char [] c = S[t].toCharArray();
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j + i < c.length; j = j + 2) {
-                System.out.print(c[j + i]);
-                }
-                System.out.print(" ");
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();  // Reads T, number of lines which will be entered
+        if (1 <= T && T <= 10){  // Constraint 1 <= T <= 10
+            scanner.nextLine();  // Skips the end of the line where T was introduced
+            String[] S = new String[T];  // String array for saving the lines
+            for (int i = 0; i < T; i++) {  // Reading of lines
+                S[i] = scanner.nextLine();
             }
-            System.out.println();
+            
+            
+            for (int t = 0; t < T; t++) { // The loop will iterate T times
+                char [] c = S[t].toCharArray();
+                if (2 <= c.length && c.length <= 10000){  // Constraint 2 <= S <= 10000
+                    for (int i = 0; i < 2; i++) {  // Twice is executed the for as we will have 2 sides
+                        for (int j = 0; j + i < c.length; j = j + 2) {  // We will increment the characters position
+                        System.out.print(c[j + i]);  // Firstly i = 0 will print even positions, i = 1 the odd ones
+                        }
+                        System.out.print(" ");  // Separator between even and odd positions
+                    }
+                    System.out.println();  // Line separator
+                }
+            }
+            
         }
+        scanner.close();
     }
 }
